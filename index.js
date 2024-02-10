dfae10ef52e89b5b4311443f4b6409fbeaeac63a
Yusuf Mehmet <107208399+TH3B35T@users.noreply.github.com>
2024-02-10 18:22:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfSquares = function(nums) {
+    let result = 0;
+    const n = nums.length;
+
+    for(let i = 0; i< n; i++){
+        if((n % (i+1)) == 0){
+            result += nums[i] * nums[i];
+        } 
+    }
+    return result;
+};
